f683993959ad1e242f47b9db61f4f58945aeb9b0
mariessad <74148632+mariessad@users.noreply.github.com>
2022-07-20 16:34:49 -0400

    create nav component, add CSS

---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img src="travel.png" className="travel--icon" />
+      <h1>My Travel Journal</h1>
+    </nav>
+  );
+}
