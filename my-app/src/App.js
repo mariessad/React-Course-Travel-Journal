c32c33e11a61d164fd994c49569cae790540e5a1
mariessad <74148632+mariessad@users.noreply.github.com>
2022-07-20 17:10:58 -0400

    create travel component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Travel from "./components/Location";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Travel />
     </div>
   );
 }
---
+++ my-app/src/components/Location.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Travel() {
+  return (
+    <div className="travel--container">
+      <img src="" alt="" className="travel--img" />
+      <section className="travel--section">
+        <p>Location</p>
+        <link></link>
+        <p>date</p>
+        <p>Description</p>
+      </section>
+    </div>
+  );
+}
