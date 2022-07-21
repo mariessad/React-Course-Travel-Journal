a5169efaafcbf19d35fadc0f65883ed1c048557f
mariessad <74148632+mariessad@users.noreply.github.com>
2022-07-20 22:23:27 -0400

    fix spelling error, add Travel location descriptions in data file

--- my-app/src/components/data.js
+++ my-app/src/components/data.js
@@ -6,7 +6,8 @@ export default [
     link: "https://goo.gl/maps/jUc53afMBD47NEFt9",
     location: "Niagra Falls",
     date: "Summer 2002",
-    description: "paragraph of details here",
+    description:
+      "Niagara Falls is a group of three waterfalls at the southern end of Niagara Gorge, spanning the border between the province of Ontario in Canada and the state of New York in the United States.",
   },
   {
     id: 2,
@@ -15,16 +16,18 @@ export default [
     link: "https://goo.gl/maps/1LJMfFChobpVyiTcA",
     location: "Tokyo",
     date: "Winter 2006",
-    description: "paragraph of details here",
+    description:
+      "Tokyo is the capital and largest city of Japan. Its metropolitan area is the most populous in the world, with an estimated 37.468 million residents in 2018.",
   },
   {
     id: 3,
-    image: "kuai.jpg",
+    image: "kauai.jpg",
     country: "Hawaii",
     link: "https://goo.gl/maps/1LJMfFChobpVyiTcA",
-    location: "Island of Kuai",
+    location: "Island of Kauai",
     date: "Summer 2012",
-    description: "paragraph of details here",
+    description:
+      "Kauai is geologically the second-oldest of the main Hawaiian Islands. It is the fourth-largest of these islands and the 21st largest island in the United States. Known also as the 'Garden Isle'",
   },
   {
     id: 4,
@@ -33,6 +36,7 @@ export default [
     link: "https://goo.gl/maps/YBtAs1FDFfjRnNw6A",
     location: "Paris",
     date: "Winter 2020",
-    description: "paragraph of details here",
+    description:
+      "Paris is the 34th most densely populated city in the world in 2020. Paris has been one of the world's major centres of finance, diplomacy, commerce, fashion, gastronomy, science, and arts.",
   },
 ];
